66ed4b909534b86208872d99085cbfade6abaddd
余勇 <2352582208@qq.com>
2026-02-02 15:12:26 +0800

    修改 rbac模块的CheckCodeFeign 远程调用 增加 "context-path"不然404

--- homi-rbac/src/main/java/com/yy/homi/rbac/feign/CheckCodeFeign.java
+++ homi-rbac/src/main/java/com/yy/homi/rbac/feign/CheckCodeFeign.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "homi-rbac")
+@FeignClient(name = "homi-third-party",path = "/homi-third-party/checkcode")
 public interface CheckCodeFeign {
 
     //校验验证码
